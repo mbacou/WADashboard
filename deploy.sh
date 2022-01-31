9cf7d803230566506b5f5e346d63dcb1ae104b3b
Melanie Bacou <mel@mbacou.com>
2022-01-31 23:16:00 +0000

    v0.1.1

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,16 @@
 
 cd "$(dirname "$(realpath "$0")")";
 
-VERSION=`git describe --abbrev=0 --tags`
-VERSION=(${VERSION//./ })
-VNUM1=${VERSION[0]}
-VNUM2=${VERSION[1]}
-VNUM2=$((VNUM2+1))
-TAG="$VNUM1.$VNUM2.0"
+if [ "$1" == "" ]
+  then
+    echo "A version tag is required to run this script"
+    exit 1
+fi
 
-git tag "v$TAG" -m "Release v$TAG"
-git commit -am "Release v$TAG" && git push
+git tag "$1" -m "Release $1"
+git commit -am "$1" && git push --tags
 
-aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 643578423538.dkr.ecr.af-south-1.amazonaws.com
-docker build -t wadashboard .
-docker tag wadashboard:latest 643578423538.dkr.ecr.af-south-1.amazonaws.com/wadashboard:latest
-docker push 643578423538.dkr.ecr.af-south-1.amazonaws.com/wadashboard:latest
+#aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 643578423538.dkr.ecr.af-south-1.amazonaws.com
+#docker build -t wadashboard .
+#docker tag wadashboard:latest 643578423538.dkr.ecr.af-south-1.amazonaws.com/wadashboard:latest
+#docker push 643578423538.dkr.ecr.af-south-1.amazonaws.com/wadashboard:latest
