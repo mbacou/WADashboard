1a2d3ae20f8c716dc2cba6dd8e42fa21918bd9a0
Melanie Bacou <mel@mbacou.com>
2022-01-31 22:42:32 +0000

    v0.1.1

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# description: - build and deploy 'wadashboard' Docker image to Amazon ECR (IWMI)
+# description: - tag, commit and deploy 'wadashboard' Docker image to Amazon ECR (IWMI)
 
 cd "$(dirname "$(realpath "$0")")";
 
 VERSION=`git describe --abbrev=0 --tags`
-VERSION_BITS=(${VERSION//./ })
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
+VERSION=(${VERSION//./ })
+VNUM1=${VERSION[0]}
+VNUM2=${VERSION[1]}
 VNUM2=$((VNUM2+1))
-NEW_TAG="$VNUM1.$VNUM2.0"
+TAG="$VNUM1.$VNUM2.0"
 
-git tag -a "$NEW_TAG" -m "Release v$NEW_TAG"
-git commit -am "Release v$NEW_TAG" && git push
+git tag -a "$TAG" -m "Release v$TAG"
+git commit -am "Release v$TAG" && git push
 
 aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 643578423538.dkr.ecr.af-south-1.amazonaws.com
 docker build -t wadashboard .
